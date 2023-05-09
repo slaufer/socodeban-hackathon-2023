c53f1e8c45586ba44ce85fde475b13abb67a9eaf
jharmonACV <jharmon@acvauctions.com>
2023-05-09 13:44:06 -0400

    Implement movement logic; add tests

--- js/socodeban.mjs
+++ js/socodeban.mjs
@@ -13,19 +13,58 @@ export default class GameBoard {
     }
 
     moveUp() {
-
+        let flipped = zip(this.contents);
+        let result = this._move(flipped, this.cursor_y, this.cursor_x);
+        if (result[1] !== this.cursor_y) {
+            this.contents = zip(result[0]);
+            this.cursor_y -= 1;
+        }
     }
 
     moveDown() {
-
+        let flipped = zip(this.contents).map(x => [...x].reverse());
+        let result = this._move(flipped, this.height - this.cursor_y - 1, this.cursor_x);
+        if (this.height - result[1] - 1 !== this.cursor_y) {
+            this.contents = zip(result[0].map(x => x.reverse()));
+            this.cursor_y += 1;
+        }
     }
 
     moveLeft() {
-
+        let result = this._move(this.contents, this.cursor_x, this.cursor_y);
+        if (result[1] !== this.cursor_x) {
+            this.contents = result[0];
+            this.cursor_x -= 1;
+        }
     }
 
     moveRight() {
+        let flipped = this.contents.map(x => [...x].reverse());
+        let result = this._move(flipped, this.width - this.cursor_x - 1, this.cursor_y);
+        if (this.width - result[1] - 1 !== this.cursor_x) {
+            this.contents = result[0].map(x => x.reverse());
+            this.cursor_x += 1;
+        }
+    }
 
+    _move(contents, cursor_x, cursor_y) {
+        let candidate = contents[cursor_y].slice(0, cursor_x);
+        let curr_x = cursor_x - 1;
+        while (1) {
+            if (curr_x < 0) {
+                return [contents, cursor_x];
+            }
+            if (candidate[curr_x] === " ") {
+                break;
+            }
+            curr_x -= 1;
+        }
+        while (curr_x < cursor_x) {
+            contents[cursor_y][curr_x] = contents[cursor_y][curr_x + 1];
+            curr_x += 1;
+        }
+        contents[cursor_y][curr_x] = ' ';
+        return [contents, cursor_x - 1];
     }
 
     validate() {
@@ -50,4 +89,10 @@ export default class GameBoard {
     arraysToString(arrs) {
         return arrs.map(x => x.join('')).join('\n');
     }
+}
+
+function zip(arrays) {
+    return arrays[0].map(function(_,i){
+        return arrays.map(function(array){return array[i]})
+    });
 }
--- js/socotest.mjs
+++ js/socotest.mjs
@@ -7,21 +7,212 @@ let teststr = "qewr\nasdf\nzxcv";
 let testarray = x.stringToArrays(teststr);
 if (testarray.length !== 3 && testarray[0].length !== 4) {
     console.log("str -> array error");
-} else { console.log("ok"); }
+} else {  process.stdout.write("."); }
 let resultstr = x.arraysToString(testarray);
 if (teststr !== resultstr) {
     console.log("array -> str errror");
-} else { console.log("ok"); }
+} else {  process.stdout.write("."); }
 
 // validate
 console.log("\nvalidation")
 x = new GameBoard('print("this is a test")', 0,0,x => x.trim() === "this is a test");
 if (! x.validate()) {
     console.log("validate positive");
-} else { console.log("ok"); }
+} else {  process.stdout.write("."); }
 
 x = new GameBoard('print("this is a test")', 0,0,x => x.trim() === "this is not a test");
 if (x.validate()) {
     console.log("validate negative");
-} else { console.log("ok"); }
+} else {  process.stdout.write("."); }
+
+// move left
+console.log("\nmove left");
+x = new GameBoard('12345\n1 3 5\n12345', 0,1,x => x);
+x.moveLeft();
+if (x.cursor_x !== 0) {
+    console.log("left cursor 0");
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1 3 5") {
+    console.log("left text 0");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 1,1,x => x);
+x.moveLeft();
+if (x.cursor_x !== 1) {
+    console.log("left cursor 1");
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1 3 5") {
+    console.log("left text 1");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 2,1,x => x);
+x.moveLeft();
+if (x.cursor_x !== 1) {
+    console.log("left cursor 2");
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "13  5") {
+    console.log("left text 2");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 3,1,x => x);
+x.moveLeft();
+if (x.cursor_x !== 2) {
+    console.log("left cursor 3");
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "13  5") {
+    console.log("left text 3");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 4,1,x => x);
+x.moveLeft();
+if (x.cursor_x !== 3) {
+    console.log("left cursor 4");
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1 35 ") {
+    console.log("left text 4");
+} else {  process.stdout.write("."); }
+
+
+// move right
+console.log("\nmove right");
+x = new GameBoard('12345\n1 3 5\n12345', 0,1,x => x);
+x.moveRight();
+if (x.cursor_x !== 1) {
+    console.log("right cursor 0 " + x.cursor_x);
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== " 13 5") {
+    console.log("right text 0 '" + x.contents[1].join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 1,1,x => x);
+x.moveRight();
+if (x.cursor_x !== 2) {
+    console.log("right cursor 1 " + x.cursor_x);
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1  35") {
+    console.log("right text 1 '" + x.contents[1].join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 2,1,x => x);
+x.moveRight();
+if (x.cursor_x !== 3) {
+    console.log("right cursor 2 " + x.cursor_x);
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1  35") {
+    console.log("right text 2 '" + x.contents[1].join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 3,1,x => x);
+x.moveRight();
+if (x.cursor_x !== 3) {
+    console.log("right cursor 3 " + x.cursor_x);
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1 3 5") {
+    console.log("right text 3 '" + x.contents[1].join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('12345\n1 3 5\n12345', 4,1,x => x);
+x.moveRight();
+if (x.cursor_x !== 4) {
+    console.log("right cursor 4 " + x.cursor_x);
+} else {  process.stdout.write("."); }
+if (x.contents[1].join('') !== "1 3 5") {
+    console.log("right text 4 '" + x.contents[1].join('') + "'");
+} else {  process.stdout.write("."); }
+
+
+// move up
+console.log("\nmove up");
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,0,x => x);
+x.moveUp();
+if (x.cursor_y !== 0) {
+    console.log("up cursor 0 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1 3 5") {
+    console.log("up text 0 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,1,x => x);
+x.moveUp();
+if (x.cursor_y !== 1) {
+    console.log("up cursor 1 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1 3 5") {
+    console.log("up text 1 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,2,x => x);
+x.moveUp();
+if (x.cursor_y !== 1) {
+    console.log("up cursor 2 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "13  5") {
+    console.log("up text 2 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,3,x => x);
+x.moveUp();
+if (x.cursor_y !== 2) {
+    console.log("up cursor 3 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "13  5") {
+    console.log("up text 3 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,4,x => x);
+x.moveUp();
+if (x.cursor_y !== 3) {
+    console.log("up cursor 4 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1 35 ") {
+    console.log("up text 4 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+
+// move down
+console.log("\nmove down");
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,0,x => x);
+x.moveDown();
+if (x.cursor_y !== 1) {
+    console.log("down cursor 0 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== " 13 5") {
+    console.log("down text 0 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,1,x => x);
+x.moveDown();
+if (x.cursor_y !== 2) {
+    console.log("down cursor 1 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1  35") {
+    console.log("down text 1 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,2,x => x);
+x.moveDown();
+if (x.cursor_y !== 3) {
+    console.log("down cursor 2 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1  35") {
+    console.log("down text 2 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,3,x => x);
+x.moveDown();
+if (x.cursor_y !== 3) {
+    console.log("down cursor 3 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1 3 5") {
+    console.log("down text 3 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
+
+x = new GameBoard('111\n2 2\n333\n4 4\n555', 1,4,x => x);
+x.moveDown();
+if (x.cursor_y !== 4) {
+    console.log("down cursor 4 " + x.cursor_y);
+} else {  process.stdout.write("."); }
+if (x.contents.map(x=>x[1]).join('') !== "1 3 5") {
+    console.log("down text 4 '" + x.contents.map(x=>x[1]).join('') + "'");
+} else {  process.stdout.write("."); }
 
